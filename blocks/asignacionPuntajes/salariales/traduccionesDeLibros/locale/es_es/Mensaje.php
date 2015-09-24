117a085baf2027f19056362da87cc79c91022055
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2015-09-23 21:48:16 -0500

    Se hace y se validan los datos del formulario registrar del bloque traduccionesDeLibros

---
+++ blocks/asignacionPuntajes/salariales/traduccionesDeLibros/locale/es_es/Mensaje.php
@@ -0,0 +1,75 @@
+<?php
+
+///************ SECCIÓN DE NOMBRES DE TABS *****************//
+$this->idioma["tabConsultar"] = "Consultar Traducciones de Libros";
+$this->idioma["tabIngresar"] = "Ingresar Traducciones de Libros";
+//////////////////////////////////////////////////////////////
+
+//************ MENSAJES *********
+$this->idioma["mensajeActualizar"] =  "Actualización Exitosa </br> Han sido actualizados los datos de la Carta al Editor para el docente <br>";
+$this->idioma["mensajeRegistro"] =  "Registro Exitoso.<br> Se ha registrado la Carta al Editor para el docente <br>" ;
+$this->idioma["mensajeError"] =  "Error al tratar de registrar la Carta al Editor.";
+$this->idioma["mensajeNoRegistros"] =  "No Se Encontraron  </br> Registros de Traducciones de Libros Para los Criterios Ingresados";
+
+/////////////////////////////////
+
+//* SECCIÓN ELEMENTOS QUE SU NOMBRE POSIBLEMENTE VARIA DENTRO DEL FORMULARIO *//
+$this->idioma["nombre"] = "Título de la Traducción:";
+$this->idioma["nombreTraductor"] = "Nombre del Traductor:";
+$this->idioma["categoria0"] = "Categorías";
+$this->idioma["categoria1"] = "Tipo de Indexación";
+$this->idioma["numero"] = "Número de Revista";
+$this->idioma["marcoConsultaGeneral"] = "Traducciones de Libros Registradas";
+$this->idioma["marcoModificarRegistro"] = "Modificar Información de Traducciones de Libros";
+
+$this->idioma["nombreTitulo"] = "Nombre de Revista Públicada.";
+$this->idioma["contextoTitulo"] = "Contexto de la Revista.";
+$this->idioma["identificadorColeccionTitulo"] = "International Standard Serial Number, Número Internacional Normalizado de Publicaciones Seriado.";
+$this->idioma["numeroTitulo"] = "Número de Publicación en Revista.";
+$this->idioma["paginasTitulo"] = "Páginas de la Publicación en Revista.";
+$this->idioma["annoTitulo"] = "Año de Publicación de la Revista.";
+
+$this->idioma["tituloArticuloTitulo"] = "Título del Artículo Indexado";
+$this->idioma["numeroAutoresTitulo"] = "Número de Autores del Artículo.";
+$this->idioma["docenteRegistrarTitulo"] = "Identificación o Nombre del Docente.";
+$this->idioma["docenteTitulo"] = "Identificación o Nombre del Docente.";
+$this->idioma["numeroAutoresUniversidadTitulo"] = "Número de Autores en la Publicacón Pertenecientes a la Universidad Distrital.";
+$this->idioma["fechaPublicacionTitulo"] = "Fecha de Publicación del Artículo.";
+$this->idioma["numeroActaTitulo"] = "Ingrese el Número de Acta CIARP-UD.";
+$this->idioma["fechaActaTitulo"] = "Seleccione la Fecha de Acta CIARP-UD.";
+$this->idioma["numeroCasoActaTitulo"] = "Ingrese el Número de Caso de Acta.";
+$this->idioma["puntajeTitulo"] = "Ingrese Puntaje.";
+///////////////////////////////////////////////////////////////////
+
+//***************** SECCIÓN BOTONES ******************//
+$this->idioma["botonRegresar"] = "Regresar";
+$this->idioma["botonConsultar"] = "Consultar";
+$this->idioma["botonGuardar"] = "Guardar";
+$this->idioma["continuar"] = "Continuar";
+$this->idioma["botonAceptar"] = "Aceptar";
+$this->idioma["botonRegistrar"] = "Registrar";
+$this->idioma["botonCancelar"] = "Cancelar";
+$this->idioma["noDefinido"] = "No definido";
+///////////////////////////////////////////////////////
+
+
+// *** SECCIÓN DE ETIQUETAS QUE POSIBLEMENTE NO VARIAN *** //
+$this->idioma["facultad"] = "Facultad:";
+$this->idioma["proyectoCurricular"] = "Proyecto Curricular:";
+$this->idioma["docente"] = "Identificación o nombre del docente:";
+$this->idioma["docenteRegistrar"] = "Identificación o Nombre del Docente:";
+$this->idioma["pais"] = "País";
+$this->idioma["anno"] = "Año:";
+$this->idioma["volumen"] = "Volumen:";
+$this->idioma["paginas"] = "Páginas";
+$this->idioma["tituloArticulo"] = "Título del Artículo";
+$this->idioma["numeroAutores"] = "Número de Autores";
+$this->idioma["numeroAutoresUniversidad"] = "Número de Autores UD";
+$this->idioma["fechaPublicacion"] = "Fecha de Traducción:";
+$this->idioma["numeroActa"] = "Número de Acta CIARP-UD:";
+$this->idioma["fechaActa"] = "Fecha de Acta CIARP-UD:";
+$this->idioma["numeroCasoActa"] = "Número de Caso de Acta:";
+$this->idioma["puntaje"] = "Puntaje:";
+////////////////////////////////////////////////////////////////
+
+?>
