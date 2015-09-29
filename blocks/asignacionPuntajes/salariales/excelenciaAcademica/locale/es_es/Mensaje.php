2009086c36496e8d2b1c392f56f8c337005804ce
Emmanuel <tabordaemmanuel@gmail.com>
2015-09-29 12:00:44 -0500

    Inicio del bloque Excelencia Académica.

---
+++ blocks/asignacionPuntajes/salariales/excelenciaAcademica/locale/es_es/Mensaje.php
@@ -0,0 +1,61 @@
+<?php
+
+///************ SECCIÓN DE NOMBRES DE TABS *****************//
+$this->idioma["tabConsultar"] = "Consultar Excelencia Académica";
+$this->idioma["tabIngresar"] = "Ingresar Excelencia Académica";
+//////////////////////////////////////////////////////////////
+
+//************ MENSAJES *********
+$this->idioma["mensajeActualizar"] =  "Actualización Exitosa </br> Han sido actualizados los datos de la Excelencia Académica <br>";
+$this->idioma["mensajeRegistro"] =  "Registro Exitoso.<br> Se ha registrado la Excelencia Académica para el docente <br>" ;
+$this->idioma["mensajeError"] =  "Error al tratar de registrar la Excelencia Académica.";
+$this->idioma["mensajeNoRegistros"] =  "No Se Encontraron  </br> Registros de Excelencia Académica </br> Para los Criterios Ingresados";
+$this->idioma["mensajeNoActualizo"] =  "Error al tratar de actualizar </br>el registro de Excelencia Académica Para el Docente </br>";
+/////////////////////////////////
+
+//* SECCIÓN ELEMENTOS QUE SU NOMBRE POSIBLEMENTE VARIA DENTRO DEL FORMULARIO *//
+
+$this->idioma["fechaResolucion"] = "Fecha de Resolución";
+$this->idioma["fechaResolucionTitulo"] = "Seleccione la Fecha de Resolución.";
+$this->idioma["tipoExperiencia"] = "Tipo de Experiencia";
+$this->idioma["tipoExperienciaTitulo"] = "Seleccione el tipo de experiencia.";
+$this->idioma["numeroResolucion"] = "Número de Resolución";
+$this->idioma["numeroResolucionTitulo"] = "Ingrese el número de resolución";
+$this->idioma["resolucionEmitidaPor"] = "Resolución Emitida Por";
+$this->idioma["resolucionEmitidaPorTitulo"] = "Ingrese la persona encargada de emitir la resolución";
+
+$this->idioma["marcoConsultaGeneral"] = "Registros de Excelencia Académica";
+$this->idioma["marcoModificarRegistro"] = "Modificar Información de la Excelencia Académica";
+
+$this->idioma["docenteRegistrarTitulo"] = "Identificación o Nombre del Docente.";
+$this->idioma["docenteTitulo"] = "Identificación o Nombre del Docente.";
+$this->idioma["numeroActaTitulo"] = "Ingrese el Número de Acta CIARP-UD.";
+$this->idioma["fechaActaTitulo"] = "Seleccione la Fecha de Acta CIARP-UD.";
+$this->idioma["numeroCasoActaTitulo"] = "Ingrese el Número de Caso de Acta.";
+$this->idioma["puntajeTitulo"] = "Ingrese el Puntaje.";
+///////////////////////////////////////////////////////////////////
+
+//***************** SECCIÓN BOTONES ******************//
+$this->idioma["botonRegresar"] = "Regresar";
+$this->idioma["botonConsultar"] = "Consultar";
+$this->idioma["botonGuardar"] = "Guardar";
+$this->idioma["continuar"] = "Continuar";
+$this->idioma["botonAceptar"] = "Aceptar";
+$this->idioma["botonRegistrar"] = "Registrar";
+$this->idioma["botonCancelar"] = "Cancelar";
+$this->idioma["noDefinido"] = "No definido";
+///////////////////////////////////////////////////////
+
+
+// *** SECCIÓN DE ETIQUETAS QUE POSIBLEMENTE NO VARIAN *** //
+$this->idioma["facultad"] = "Facultad";
+$this->idioma["proyectoCurricular"] = "Proyecto Curricular";
+$this->idioma["docente"] = "Indentificación o nombre del docente:";
+$this->idioma["docenteRegistrar"] = "Identificación o Nombre del Docente";
+$this->idioma["numeroActa"] = "Número de Acta CIARP-UD";
+$this->idioma["fechaActa"] = "Fecha de Acta CIARP-UD";
+$this->idioma["numeroCasoActa"] = "Número de Caso de Acta";
+$this->idioma["puntaje"] = "Puntaje	";
+////////////////////////////////////////////////////////////////
+
+?>
