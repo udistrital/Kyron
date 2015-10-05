ed0191b3788ab5934e83520dfb0345fa864bb225
Emmanuel Taborda <tabordaemmanuel@gmail.com>
2015-10-05 17:03:09 -0500

    Avance módulo Premios Otorgados al Docente.

---
+++ blocks/asignacionPuntajes/salariales/premiosDocente/Sql.class.php
@@ -0,0 +1,295 @@
+<?php
+
+namespace asignacionPuntajes\salariales\premiosDocente;
+
+if (! isset ( $GLOBALS ["autorizado"] )) {
+	include ("../index.php");
+	exit ();
+}
+
+include_once ("core/manager/Configurador.class.php");
+include_once ("core/connection/Sql.class.php");
+
+// Para evitar redefiniciones de clases el nombre de la clase del archivo sqle debe corresponder al nombre del bloque
+// en camel case precedida por la palabra sql
+class Sql extends \Sql {
+	var $miConfigurador;
+	function __construct() {
+		$this->miConfigurador = \Configurador::singleton ();
+	}
+	function getCadenaSql($tipo, $variable = "") {
+		
+		/**
+		 * 1.
+		 * Revisar las variables para evitar SQL Injection
+		 */
+		$prefijo = $this->miConfigurador->getVariableConfiguracion ( "prefijo" );
+		$idSesion = $this->miConfigurador->getVariableConfiguracion ( "id_sesion" );
+		
+		switch ($tipo) {
+			
+			/**
+			 * Clausulas genéricas.
+			 * se espera que estén en todos los formularios
+			 * que utilicen esta plantilla
+			 */
+			case "iniciarTransaccion" :
+				$cadenaSql = "START TRANSACTION";
+				break;
+			
+			case "finalizarTransaccion" :
+				$cadenaSql = "COMMIT";
+				break;
+			
+			case "cancelarTransaccion" :
+				$cadenaSql = "ROLLBACK";
+				break;
+			
+			case "eliminarTemp" :
+				
+				$cadenaSql = "DELETE ";
+				$cadenaSql .= "FROM ";
+				$cadenaSql .= $prefijo . "tempFormulario ";
+				$cadenaSql .= "WHERE ";
+				$cadenaSql .= "id_sesion = '" . $variable . "' ";
+				break;
+			
+			case "insertarTemp" :
+				$cadenaSql = "INSERT INTO ";
+				$cadenaSql .= $prefijo . "tempFormulario ";
+				$cadenaSql .= "( ";
+				$cadenaSql .= "id_sesion, ";
+				$cadenaSql .= "formulario, ";
+				$cadenaSql .= "campo, ";
+				$cadenaSql .= "valor, ";
+				$cadenaSql .= "fecha ";
+				$cadenaSql .= ") ";
+				$cadenaSql .= "VALUES ";
+				
+				foreach ( $_REQUEST as $clave => $valor ) {
+					$cadenaSql .= "( ";
+					$cadenaSql .= "'" . $idSesion . "', ";
+					$cadenaSql .= "'" . $variable ['formulario'] . "', ";
+					$cadenaSql .= "'" . $clave . "', ";
+					$cadenaSql .= "'" . $valor . "', ";
+					$cadenaSql .= "'" . $variable ['fecha'] . "' ";
+					$cadenaSql .= "),";
+				}
+				
+				$cadenaSql = substr ( $cadenaSql, 0, (strlen ( $cadenaSql ) - 1) );
+				break;
+			
+			case "rescatarTemp" :
+				$cadenaSql = "SELECT ";
+				$cadenaSql .= "id_sesion, ";
+				$cadenaSql .= "formulario, ";
+				$cadenaSql .= "campo, ";
+				$cadenaSql .= "valor, ";
+				$cadenaSql .= "fecha ";
+				$cadenaSql .= "FROM ";
+				$cadenaSql .= $prefijo . "tempFormulario ";
+				$cadenaSql .= "WHERE ";
+				$cadenaSql .= "id_sesion='" . $idSesion . "'";
+				break;
+			
+			/* Consultas del desarrollo */
+			case "facultad" :
+				$cadenaSql = "SELECT";
+				$cadenaSql .= " id_facultad,";
+				$cadenaSql .= "	nombre";
+				$cadenaSql .= " FROM ";
+				$cadenaSql .= " docencia.facultad";
+				break;
+				
+			case "proyectoCurricular" :
+				$cadenaSql = "SELECT";
+				$cadenaSql .= " id_proyectocurricular,";
+				$cadenaSql .= "	nombre";
+				$cadenaSql .= " FROM ";
+				$cadenaSql .= " docencia.proyectocurricular";
+				break;
+				
+			case "entidadInstitucion" :
+				$cadenaSql = "SELECT";
+				$cadenaSql .= " id_universidad,";
+				$cadenaSql .= "	nombre_universidad";
+				$cadenaSql .= " FROM ";
+				$cadenaSql .= " docencia.universidad";
+				break;
+				
+			case "tipoEntidadInstitucion" :
+				$cadenaSql = "SELECT";
+				$cadenaSql .= " id_tipo_entidad,";
+				$cadenaSql .= "	nombre_tipo_entidad";
+				$cadenaSql .= " FROM ";
+				$cadenaSql .= " docencia.tipo_entidad";
+				break;
+				
+			case "contexto" :
+				$cadenaSql = "select";
+				$cadenaSql .= " id_contexto,";
+				$cadenaSql .= "	descripcion";
+				$cadenaSql .= " FROM ";
+				$cadenaSql .= " docencia.contexto";
+				break;
+				
+			case "pais" :
+				$cadenaSql = "SELECT";
+				$cadenaSql .= " paiscodigo,";
+				$cadenaSql .= "	paisnombre";
+				$cadenaSql .= " FROM ";
+				$cadenaSql .= " docencia.pais";
+				if($variable == 1){
+					$cadenaSql .= " WHERE paiscodigo = 'COL'";
+				}elseif ($variable == 2){
+					$cadenaSql .= " WHERE paiscodigo != 'COL'";
+				}
+				$cadenaSql .= "order by paisnombre";
+				break;
+				
+			case "ciudad" :
+				$cadenaSql = "SELECT";
+				$cadenaSql .= " ciudadid,";
+				$cadenaSql .= "	ciudadnombre";
+				$cadenaSql .= " FROM ";
+				$cadenaSql .= " docencia.ciudad";
+				$cadenaSql .= " WHERE paiscodigo ='" . $variable ."'";
+				break;
+				
+			case "categoria_revista" :
+				$cadenaSql = "select";
+				$cadenaSql .= " id_tipo_indexacion,";
+				$cadenaSql .= "	descripcion";
+				$cadenaSql .= " FROM ";
+				$cadenaSql .= " docencia.tipo_indexacion";
+				$cadenaSql .= " WHERE";
+				$cadenaSql .= " id_contexto =" . $variable;
+				break;
+				
+			case "docente" :
+				$cadenaSql=" SELECT";
+				$cadenaSql.=" documento_docente||' - '||primer_nombre||' '||segundo_nombre||' '||primer_apellido||' '||segundo_apellido AS value, ";
+				$cadenaSql.=" documento_docente AS data ";
+				$cadenaSql.=" FROM ";
+				$cadenaSql.=" docencia.docente WHERE documento_docente||' - '||primer_nombre||' '||segundo_nombre||' '||primer_apellido||' '||segundo_apellido ";
+				$cadenaSql.=" LIKE '%" . $variable . "%' LIMIT 10;";
+				break;
+								
+			case "consultar" :			
+				$cadenaSql=" select ";
+				$cadenaSql.=" ce.documento_docente, ";
+				$cadenaSql.=" dc.primer_nombre||' '||dc.segundo_nombre||' '||dc.primer_apellido||' '||dc.segundo_apellido nombre_docente,";
+				$cadenaSql.=" ce.nombre_revista, ce.titulo_articulo, pi.paisnombre, ti.descripcion as tipo_indexacion,";
+				$cadenaSql.=" ce.numero_issn, ce.anno_publicacion,";
+				$cadenaSql.=" ce.volumen_revista, ce.numero_revista,";
+				$cadenaSql.=" ce.paginas_revista,";
+				$cadenaSql.=" ce.fecha_publicacion ";
+				$cadenaSql.=" from ";
+				$cadenaSql.=" docencia.cartas_editor ce ";
+				$cadenaSql.=" left join docencia.docente dc on ce.documento_docente=dc.documento_docente ";
+				$cadenaSql.=" left join docencia.docente_proyectocurricular dc_pc on ce.documento_docente=dc_pc.documento_docente ";
+				$cadenaSql.=" left join docencia.proyectocurricular pc on dc_pc.id_proyectocurricular=pc.id_proyectocurricular ";
+				$cadenaSql.=" left join docencia.facultad fc on pc.id_facultad=fc.id_facultad ";
+				$cadenaSql.=" left join docencia.pais pi on ce.paiscodigo=pi.paiscodigo ";
+				$cadenaSql.=" left join docencia.tipo_indexacion ti ON ti.id_tipo_indexacion = ce.id_tipo_indexacion";
+				$cadenaSql.=" where ce.estado=true";
+				$cadenaSql.=" and dc.estado=true";
+				$cadenaSql.=" and pc.estado=true";
+				$cadenaSql.=" and dc_pc.estado=true";
+				if ($variable ['documento_docente'] != '') {
+					$cadenaSql .= " AND dc.documento_docente = '" . $variable ['documento_docente'] . "'";
+				}
+				if ($variable ['id_facultad'] != '') {
+					$cadenaSql .= " AND fc.id_facultad = '" . $variable ['id_facultad'] . "'";
+				}
+				if ($variable ['id_proyectocurricular'] != '') {
+					$cadenaSql .= " AND pc.id_proyectocurricular = '" . $variable ['id_proyectocurricular'] . "'";
+				}
+				break;
+				
+			case "registrar" :
+				$cadenaSql = "INSERT INTO docencia.cartas_editor( ";
+				$cadenaSql .= "documento_docente, nombre_revista, id_contexto, paiscodigo, ";
+				$cadenaSql .= "id_tipo_indexacion, ";
+				$cadenaSql .= "numero_issn, anno_publicacion, volumen_revista, numero_revista, paginas_revista, ";
+				$cadenaSql .= "titulo_articulo, numero_autores, numero_autores_ud, fecha_publicacion, ";
+				$cadenaSql .= "numero_acta, fecha_acta, numero_caso, puntaje) ";
+				$cadenaSql .= " VALUES (" . $variable ['id_docenteRegistrar'] . ",";
+				$cadenaSql .= " '" . $variable ['nombre'] . "',";
+				$cadenaSql .= " '" . $variable ['contexto'] . "',";
+				$cadenaSql .= "'" . $variable ['pais'] . "',";
+				$cadenaSql .= " '" . $variable ['categoria'] . "',";
+				$cadenaSql .= " '" . $variable ['identificadorColeccion'] . "',";
+				$cadenaSql .= " '" . $variable ['anno'] . "',";
+				$cadenaSql .= " '" . $variable ['volumen'] . "',";
+				$cadenaSql .= " '" . $variable ['numero'] . "',";
+				$cadenaSql .= " '" . $variable ['paginas'] . "',";
+				$cadenaSql .= " '" . $variable ['tituloArticulo'] . "',";
+				$cadenaSql .= " '" . $variable ['numeroAutores'] . "',";
+				$cadenaSql .= " '" . $variable ['numeroAutoresUniversidad'] . "',";
+				$cadenaSql .= "' " . $variable ['fechaPublicacion'] . "' ,";
+				$cadenaSql .= "' " . $variable ['numeroActa'] . "',";
+				$cadenaSql .= " '" . $variable ['fechaActa'] . "',";
+				$cadenaSql .= "' " . $variable ['numeroCasoActa'] . "',";
+				$cadenaSql .= " '" . $variable ['puntaje'] . "')";
+				break;
+				
+			case "publicacionActualizar" :
+				$cadenaSql=" SELECT ce.documento_docente,";
+				$cadenaSql.=" dc.primer_nombre||' '||dc.segundo_nombre||' '||dc.primer_apellido||' '||dc.segundo_apellido nombre_docente,";
+				$cadenaSql.=" ce.nombre_revista, ";
+				$cadenaSql.=" ce.id_contexto, ";
+				$cadenaSql.=" ce.paiscodigo, ";
+				$cadenaSql.=" ce.id_tipo_indexacion, ";
+				$cadenaSql.=" ce.numero_issn, ";
+				$cadenaSql.=" ce.anno_publicacion, ";
+				$cadenaSql.=" ce.volumen_revista, ";
+				$cadenaSql.=" ce.numero_revista, ";
+				$cadenaSql.=" ce.paginas_revista, ";
+				$cadenaSql.=" ce.titulo_articulo, ";
+				$cadenaSql.=" ce.numero_autores, ";
+				$cadenaSql.=" ce.numero_autores_ud, ";
+				$cadenaSql.=" ce.fecha_publicacion, ";
+				$cadenaSql.=" ce.numero_acta, ";
+				$cadenaSql.=" ce.fecha_acta, ";
+				$cadenaSql.=" ce.numero_caso, ";
+				$cadenaSql.=" ce.puntaje ";
+				$cadenaSql.=" FROM docencia.cartas_editor ce ";
+				$cadenaSql.=" left join docencia.docente dc on ce.documento_docente=dc.documento_docente ";
+				$cadenaSql.=" WHERE ce.documento_docente ='" . $variable['documento_docente']. "'";
+				$cadenaSql.=" and ce.estado=true";
+				$cadenaSql.=" and ce.numero_issn ='" . $variable['identificadorColeccion']. "'";
+				break;
+				
+			case "actualizar" :
+				$cadenaSql = "UPDATE ";
+				$cadenaSql .= "docencia.cartas_editor ";
+				$cadenaSql .= "SET ";
+				$cadenaSql .= "nombre_revista = '" . $variable ['nombre'] . "', ";
+				$cadenaSql .= "id_contexto = '" . $variable ['contexto'] . "', ";
+				$cadenaSql .= "paiscodigo = '" . $variable ['pais'] . "', ";
+				$cadenaSql .= "id_tipo_indexacion = '" . $variable ['categoria'] . "', ";
+				$cadenaSql .= "numero_issn = '" . $variable ['identificadorColeccion'] . "', ";
+				$cadenaSql .= "anno_publicacion = '" . $variable ['anno'] . "', ";
+				$cadenaSql .= "volumen_revista = '" . $variable ['volumen'] . "', ";
+				$cadenaSql .= "numero_revista = '" . $variable ['numero'] . "', ";
+				$cadenaSql .= "paginas_revista = '" . $variable ['paginas'] . "', ";
+				$cadenaSql .= "titulo_articulo = '" . $variable ['tituloArticulo'] . "', ";
+				$cadenaSql .= "numero_autores = '" . $variable ['numeroAutores'] . "', ";
+				$cadenaSql .= "numero_autores_ud = '" . $variable ['numeroAutoresUniversidad'] . "', ";
+				$cadenaSql .= "fecha_publicacion = '" . $variable ['fechaPublicacion'] . "', ";
+				$cadenaSql .= "numero_acta = '" . $variable ['numeroActa'] . "', ";
+				$cadenaSql .= "fecha_acta = '" . $variable ['fechaActa'] . "', ";
+				$cadenaSql .= "numero_caso = '" . $variable ['numeroCasoActa'] . "', ";
+				$cadenaSql .= "puntaje = '" . $variable ['puntaje'] . "'";
+				$cadenaSql .= "WHERE ";
+				$cadenaSql .= "documento_docente ='" . $variable ['id_docenteRegistrar'] . "' ";
+				$cadenaSql .= "and numero_issn ='" . $variable ['identificadorColeccion_old'] . "' ";
+				break;
+		}
+		
+		return $cadenaSql;
+	}
+}
+
+?>
---
+++ blocks/asignacionPuntajes/salariales/premiosDocente/script/ready.php
@@ -0,0 +1,165 @@
+$("#premiosDocente").validationEngine({
+promptPosition : "centerRight", 
+scroll: false,
+autoHidePrompt: true,
+autoHideDelay: 2000
+});
+
+$(function() {
+	$("#premiosDocente").submit(function() {
+		$resultado=$("#premiosDocente").validationEngine("validate");
+		if ($resultado) {
+			return true;
+		}
+		return false;
+	});
+});
+
+$("#premiosDocenteRegistrar").validationEngine({
+	promptPosition : "centerRight", 
+	scroll: false,
+	autoHidePrompt: true,
+	autoHideDelay: 2000
+});
+
+$(function() {
+$("#premiosDocenteRegistrar").submit(function() {
+$resultado=$("#premiosDocenteRegistrar").validationEngine("validate");
+
+if ($resultado) {
+
+return true;
+}
+return false;
+});
+});
+
+$(function () {
+    $("button").button().click(function (event) { 
+        event.preventDefault();
+    });
+});
+
+$(function() {
+	$("#premiosDocenteModificar").submit(function() {
+		$resultado=$("#premiosDocenteModificar").validationEngine("validate");
+		if ($resultado) {
+			return true;
+		}
+		return false;
+	});
+});
+
+$("#premiosDocenteModificar").validationEngine({
+	promptPosition : "centerRight", 
+	scroll: false,
+	autoHidePrompt: true,
+	autoHideDelay: 2000
+});
+
+$('#tablaTitulos').dataTable( {
+	"sPaginationType": "full_numbers"
+});
+        
+////////////Función que organiza los tabs en la interfaz gráfica//////////////
+$(function() {
+	$("#tabs").tabs();
+}); 
+//////////////////////////////////////////////////////////////////////////////
+
+// Asociar el widget de validación al formulario
+
+/////////Se define el ancho de los campos de listas desplegables///////
+$('#<?php echo $this->campoSeguro('docente')?>').width(465);      
+$('#<?php echo $this->campoSeguro('facultad')?>').width(450);      
+$('#<?php echo $this->campoSeguro('proyectoCurricular')?>').width(450);      
+
+$('#<?php echo $this->campoSeguro('docenteRegistrar')?>').width(465);
+$('#<?php echo $this->campoSeguro('contexto')?>').width(450);
+$('#<?php echo $this->campoSeguro('anno')?>').width(450);
+$("#<?php echo $this->campoSeguro('entidad')?>").width(450);
+$("#<?php echo $this->campoSeguro('tipoEntidad')?>").width(450);
+
+
+///*********El ancho (width) de los siguientes campos es mayor debido a que se encuentran dentro de un div****///
+$('#<?php echo $this->campoSeguro('pais')?>').width(470);
+$('#<?php echo $this->campoSeguro('categoria')?>').width(470);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////**********Se definen los campos que requieren campos de select2**********////////////////
+
+$("#<?php echo $this->campoSeguro('facultad')?>").select2();
+$("#<?php echo $this->campoSeguro('proyectoCurricular')?>").select2();
+
+$("#<?php echo $this->campoSeguro('entidad')?>").select2();
+$("#<?php echo $this->campoSeguro('tipoEntidad')?>").select2();
+$("#<?php echo $this->campoSeguro('contexto')?>").select2();
+$("#<?php echo $this->campoSeguro('anno')?>").select2();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+$("#<?php echo $this->campoSeguro('entidad')?>").change(function() {
+	if($("#<?php echo $this->campoSeguro('entidad')?>").val()==''){
+		$("#<?php echo $this->campoSeguro('otraEntidad')?>").removeAttr("disabled");
+		var idDatosEstudiantes = [
+                       	 '<?php echo $this->campoSeguro('otraEntidad')?>',
+                       ];
+		//Agrega required a todos los campos de los estudiantes
+		$.each(idDatosEstudiantes,function (i,v){
+			var obj = $("#"+v);
+			if(obj.length>0){
+				var clases = obj.attr('class').split(' ');
+				var claseValidate = $.grep(clases,function(v,i){return v.indexOf('validate')>-1})[0];
+				obj.removeClass(claseValidate);
+				claseValidate = claseValidate.insertAt(claseValidate.indexOf("[")+1,'required,');
+				obj.addClass(claseValidate);
+			}
+		});
+	}else{
+		$("#<?php echo $this->campoSeguro('otraEntidad')?>").attr("disabled", "disabled");
+		$("#<?php echo $this->campoSeguro('otraEntidad')?>").val("");
+		var idDatosEstudiantes = [
+                       	 '<?php echo $this->campoSeguro('entidad')?>',
+                       ];
+		//Agrega required a todos los campos de los estudiantes
+		$.each(idDatosEstudiantes,function (i,v){
+			var obj = $("#"+v);
+			if(obj.length>0){
+				var clases = obj.attr('class').split(' ');
+				var claseValidate = $.grep(clases,function(v,i){return v.indexOf('validate')>-1})[0];
+				obj.removeClass(claseValidate);
+				claseValidate = claseValidate.insertAt(claseValidate.indexOf("[")+1,'required,');
+				obj.addClass(claseValidate);
+			}
+		});
+	}
+});
+
+if($("#<?php echo $this->campoSeguro('entidad')?>").val()!=''){
+		$("#<?php echo $this->campoSeguro('otraEntidad')?>").attr("disabled", "disabled");
+}else{
+
+		String.prototype.insertAt=function(index, string) { 
+	  		return this.substr(0, index) + string + this.substr(index);
+		}
+		var idDatosEstudiantes = [
+                       	 '<?php echo $this->campoSeguro('otraEntidad')?>',
+                       ];
+		//Agrega required a todos los campos de los estudiantes
+		$.each(idDatosEstudiantes,function (i,v){
+			var obj = $("#"+v);
+			if(obj.length>0){
+				var clases = obj.attr('class').split(' ');
+				var claseValidate = $.grep(clases,function(v,i){return v.indexOf('validate')>-1})[0];
+				obj.removeClass(claseValidate);
+				claseValidate = claseValidate.insertAt(claseValidate.indexOf("[")+1,'required,');
+				obj.addClass(claseValidate);
+			}
+		});
+}
+
+String.prototype.insertAt=function(index, string) { 
+	  return this.substr(0, index) + string + this.substr(index);
+	}
