3a5dfb78644b20378f3be48c9b0589a90c4dacbb
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2015-10-14 21:32:43 -0500

    bloque inicial novedadesBonificacion

---
+++ blocks/asignacionPuntajes/novedades/novedadesBonificacion/locale/es_es/Mensaje.php
@@ -0,0 +1,62 @@
+<?php
+
+///************ SECCIÓN DE NOMBRES DE TABS *****************//
+$this->idioma["tabConsultar"] = "Consultar Novedades Salariales";
+$this->idioma["tabIngresar"] = "Ingresar Novedades Salariales";
+//Tooltip formulario
+$this->idioma["resenaCriticaRegistrar"] = "";
+//////////////////////////////////////////////////////////////
+
+//************ MENSAJES *********
+$this->idioma["mensajeActualizar"] =  "Actualización Exitosa </br> Han sido actualizados los datos de la Novedades Salariales <br>";
+$this->idioma["mensajeRegistro"] =  "Registro Exitoso.<br> Se ha registrado la Novedades Salariales para el docente <br>" ;
+$this->idioma["mensajeError"] =  "Error al tratar de registrar la Novedades Salariales.";
+$this->idioma["mensajeNoRegistros"] =  "No Se Encontraron  </br> Registros de Novedades Salariales </br> Para los Criterios Ingresados";
+$this->idioma["mensajeNoActualizo"] =  "Error al tratar de actualizar </br>el registro de Novedades Salariales Para el Docente </br>";
+/////////////////////////////////
+
+//* SECCIÓN ELEMENTOS QUE SU NOMBRE POSIBLEMENTE VARIA DENTRO DEL FORMULARIO *//
+$this->idioma["descripcion"] = "Descripción de la novedad:";
+$this->idioma["tipo"] = "Asignación Puntaje:";
+
+$this->idioma["descripcionTitulo"] = "Ingrese el título de la novedad.";
+$this->idioma["tipoTitulo"] = "Ingrese el item o módulo para el que se reporta la novedad.";
+
+$this->idioma["marcoConsultaGeneral"] = "Novedades Salariales";
+$this->idioma["marcoModificarRegistro"] = "Modificar Información de la Novedades Salariales";
+$this->idioma["marcoModificarRegistroTitulo"] = "";
+
+$this->idioma["docenteRegistrarTitulo"] = "Identificación o Nombre del Docente.";
+$this->idioma["docenteTitulo"] = "Identificación o Nombre del Docente.";
+$this->idioma["numeroActaTitulo"] = "Ingrese el Número de Acta CIARP-UD.";
+$this->idioma["fechaActaTitulo"] = "Seleccione la Fecha de Acta CIARP-UD.";
+$this->idioma["numeroCasoActaTitulo"] = "Ingrese el Número de Caso de Acta.";
+$this->idioma["puntajeTitulo"] = "Ingrese el Puntaje.";
+$this->idioma["normatividadTitulo"] = "Ingrese la normatividad.";
+///////////////////////////////////////////////////////////////////
+
+//***************** SECCIÓN BOTONES ******************//
+$this->idioma["botonRegresar"] = "Regresar";
+$this->idioma["botonConsultar"] = "Consultar";
+$this->idioma["botonGuardar"] = "Guardar";
+$this->idioma["continuar"] = "Continuar";
+$this->idioma["botonAceptar"] = "Aceptar";
+$this->idioma["botonRegistrar"] = "Registrar";
+$this->idioma["botonCancelar"] = "Cancelar";
+$this->idioma["noDefinido"] = "No definido";
+///////////////////////////////////////////////////////
+
+
+// *** SECCIÓN DE ETIQUETAS QUE POSIBLEMENTE NO VARIAN *** //
+$this->idioma["facultad"] = "Facultad:";
+$this->idioma["proyectoCurricular"] = "Proyecto Curricular:";
+$this->idioma["docente"] = "Identificación o nombre del docente:";
+$this->idioma["docenteRegistrar"] = "Identificación o nombre del docente:";
+$this->idioma["numeroActa"] = "Número de Acta CIARP-UD:";
+$this->idioma["fechaActa"] = "Fecha de Acta CIARP-UD:";
+$this->idioma["numeroCasoActa"] = "Número de Caso de Acta:";
+$this->idioma["puntaje"] = "Puntaje:";
+$this->idioma["normatividad"] = "Normatividad:";
+////////////////////////////////////////////////////////////////
+
+?>
