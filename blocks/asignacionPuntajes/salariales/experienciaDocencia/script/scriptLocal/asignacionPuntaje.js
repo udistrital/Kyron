12b1f339ca93de1023e4e42589894e110d625e63
Emmanuel Taborda <tabordaemmanuel@gmail.com>
2015-10-19 11:00:15 -0500

    Asignación Puntaje capítulo experiencia docencia.

---
+++ blocks/asignacionPuntajes/salariales/experienciaDocencia/script/scriptLocal/asignacionPuntaje.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+
+	/*SEGUN EL CASO DE USO 14. REGISTRAR EXPERIENCIA EN DIRECCIÓN ACADEMICA
+	 * 
+	 * Puntaje asignado (4 puntos salariales por un año de Experiencia 
+	 *y el máximo de puntaje  de acuerdo a la relación duración en días con respecto a los al año de experiencia)
+     *4 Puntos maximos-------->1 Año (365 dias)
+     *(Puntos maximos)---------> Duracion(dias) 
+     *un dia equivale a 0.01095890411*
+     *Para calcular el máximo puntaje que se puede asignar se divide 4 que es el máximo puntaje por año
+     *y se divide en 365 días dando como resultado el factor por el que se debe multiplicar cada día
+     *para ir incrementando el puntaje máximo dependiendo de la cantidad de días que ingrese como expeiencia.*/
+
+	var diasExperiencia;
+	var puntajeAnnio = 4;
+	asignarPuntaje();
+	
+	$("#<?php echo $this->campoSeguro('duracionExperiencia')?>").blur(function() {
+		asignarPuntaje();
+	});
+		
+	function asignarPuntaje(){
+		
+		diasExperiencia = $("#<?php echo $this->campoSeguro('duracionExperiencia')?>").val();
+		puntajeMaximo = redondeo(diasExperiencia*(puntajeAnnio/365), 3);
+		$("#<?php echo $this->campoSeguro('puntaje')?>").attr("class", "cuadroTexto ui-widget ui-widget-content ui-corner-all   validate[required, custom[number],min[0.1],max["+puntajeMaximo+"]");
+	}
+	
+	function redondeo(numero, decimales)
+	{
+	var flotante = parseFloat(numero);
+	var resultado = Math.round(flotante*Math.pow(10,decimales))/Math.pow(10,decimales);
+	return resultado;
+	}
+
+});
