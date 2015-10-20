84aa837f0cd0687159ed44e28a739e8cf686a341
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2015-10-19 22:08:44 -0500

    borrar directorio mal puesto

--- blocks/asignacionPuntajes/asignacionPuntajes/salariales/excelenciaAcademica/script/scriptLocal/asignacionPuntaje.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-
-	asignarPuntaje();
-
-	function asignarPuntaje(){
-		$("#<?php echo $this->campoSeguro('puntaje')?>").attr("class", "cuadroTexto ui-widget ui-widget-content ui-corner-all   validate[required, custom[number],min[0.1],max[2]]");
-	}
-});
