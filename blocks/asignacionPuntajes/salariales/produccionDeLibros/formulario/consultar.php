7413e9c22702f4845569ec8745bcfaaf1bed52e3
Emmanuel Taborda <tabordaemmanuel@gmail.com>
2015-12-17 11:48:33 -0500

    Ajuste al módulo producción de libros, solicitado por docencia.

--- blocks/asignacionPuntajes/salariales/produccionDeLibros/formulario/consultar.php
+++ blocks/asignacionPuntajes/salariales/produccionDeLibros/formulario/consultar.php
@@ -174,6 +174,7 @@ class registrarForm {
 						<th>Fecha Acta</th>
 						<th>Número Caso</th>
 						<th>Puntaje</th>
+						<th>Normatividad</th>
 						<th>Modificar</th>
 	                </tr>
 	            </thead>
@@ -202,6 +203,7 @@ class registrarForm {
 	                    <td><center>" . $indexacion [$i] ['fecha_acta'] . "</center></td>
 	                    <td><center>" . $indexacion [$i] ['numero_caso'] . "</center></td>
 	                    <td><center>" . $indexacion [$i] ['puntaje'] . "</center></td>
+	                    <td><center>" . $indexacion [$i] ['normatividad'] . "</center></td>
 	                    <td><center>
 	                    	<a href='" . $variable . "'>
 	                            <img src='" . $rutaBloque . "/css/images/Entrada.png' width='15px'>
