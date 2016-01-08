7077c4a4fc9c42b36aa42e3fefde4730afad2b9f
Emmanuel Taborda <tabordaemmanuel@gmail.com>
2016-01-07 23:27:11 -0500

    Ajustes 2

--- blocks/asignacionPuntajes/salariales/direccionTrabajosDeGrado/Funcion.class.php
+++ blocks/asignacionPuntajes/salariales/direccionTrabajosDeGrado/Funcion.class.php
@@ -80,7 +80,6 @@ class Funcion {
 		 */
 		if(isset($_REQUEST['validadorCampos']) && !(isset($_REQUEST ["botonRegresar"]) && $_REQUEST ["botonRegresar"] == 'true')){
 			$validadorCampos = $this->miInspectorHTML->decodificarCampos($_REQUEST['validadorCampos']);
-			echo($_REQUEST['validadorCampos']);
 			$respuesta = $this->miInspectorHTML->validacionCampos($_REQUEST,$validadorCampos,false,false);
 			if ($respuesta != false){
 				$_REQUEST = $respuesta;
