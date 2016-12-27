77c63921753e01831d469beb64373408d3f3cc2f
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-12-27 09:16:16 -0500

    Se corrige bloque estadoDeCuentaCondor

--- blocks/reportes/estadoDeCuentaCondor/builder/html/item.html.php
+++ blocks/reportes/estadoDeCuentaCondor/builder/html/item.html.php
@@ -1,11 +1,11 @@
 <?php foreach($items as $item):?>
-<?php if(count($item['resultados'])!=0):?>
+<?php if(count($item['_resultados'])!=0):?>
 <br />
 <center>
 	<table width='100%'>
 		<tr>
 			<th colspan='4' style='font-size: 11px; text-align: left'>
-				<?php echo $item['tituloTipo'] . ': ' . $item['titulo']; ?>			
+				<?php echo $item['_tituloTipo'] . ': ' . $item['_titulo']; ?>			
 			</th>
 		</tr>
 		<tr>
@@ -17,10 +17,10 @@
 		<?php $index = 1;
 			$puntajeTotal = 0;
 		?>
-		<?php foreach ($item['resultados'] as $resultado):?>
+		<?php foreach ($item['_resultados'] as $resultado):?>
 		<tr>
 			<td>
-				<?php foreach ($item['descripcion'] as $descripcion):?>
+				<?php foreach ($item['_descripcion'] as $descripcion):?>
 					<?php echo $descripcion['alias_campo'] . ': ' . $resultado[$descripcion['nombre_campo']]. ';'; ?>
 				<?php endforeach;?>
 			</td>
@@ -41,9 +41,9 @@
 			<th colspan='3f' style='font-size: 10px; text-align: right'>Subtotal</th>
 			<td width='50px'><?php echo $puntajeTotal; ?></td>
 			<?php
-			if ($item['tipo'] == '1') {//Salarial
+			if ($item['_tipo'] == '1') {//Salarial
 				$puntosSalariales += $puntajeTotal;
-			} elseif ($item['tipo'] == '2') {//Bonficación
+			} elseif ($item['_tipo'] == '2') {//Bonficación
 				$puntosBonificacion += $puntajeTotal;
 			}
 			?>
