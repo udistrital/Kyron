a1ec48adf7e230181c3a5bbb5a2038de21656dbb
Jorge Useche <naturalmentejorge@gmail.com>
2017-06-02 14:38:17 -0500

    Se corrigen algunos cambios aprobados por el usuario.

--- blocks/asignacionPuntajes/bonificacion/publicacionesImpresas/formulario/tabs/tabRegistrar.php
+++ blocks/asignacionPuntajes/bonificacion/publicacionesImpresas/formulario/tabs/tabRegistrar.php
@@ -173,12 +173,12 @@ class FormularioRegistro {
 		$atributos ['estilo'] = 'jqueryui';
 		$atributos ['marco'] = true;
 		$atributos ['estiloMarco'] = '';
-		$atributos ["etiquetaObligatorio"] = true;
+		$atributos ['etiquetaObligatorio'] = false;
 		$atributos ['columnas'] = 1;
 		$atributos ['dobleLinea'] = 0;
 		$atributos ['tabIndex'] = $tab;
 		$atributos ['etiqueta'] = $this->lenguaje->getCadena ( $esteCampo );
-		$atributos ['validar'] = 'required, minSize[8],maxSize[30]';
+		$atributos ['validar'] = 'minSize[8],maxSize[30]';
 	
 		if (isset ( $_REQUEST [$esteCampo] )) {
 			$atributos ['valor'] = $_REQUEST [$esteCampo];
