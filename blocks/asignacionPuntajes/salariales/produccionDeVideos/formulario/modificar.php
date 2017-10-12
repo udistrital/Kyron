28fdc2a4914186e0c6e0b2153e78a44f31a6eb21
Jorge Useche <naturalmentejorge@gmail.com>
2017-10-12 01:33:38 -0500

    Se quita campo autores ud como requerido.

--- blocks/asignacionPuntajes/salariales/produccionDeVideos/formulario/modificar.php
+++ blocks/asignacionPuntajes/salariales/produccionDeVideos/formulario/modificar.php
@@ -268,7 +268,7 @@ class FormularioModificar {
 				$atributos ['dobleLinea'] = 0;
 				$atributos ['tabIndex'] = $tab;
 				$atributos ['etiqueta'] = $this->lenguaje->getCadena ( $esteCampo );
-				$atributos ['validar'] = 'required, custom[onlyNumberSp], maxSize[2]';
+				$atributos ['validar'] = 'custom[onlyNumberSp], maxSize[2]';
 					
 				if (isset ( $_REQUEST [$esteCampo] )) {
 					$atributos ['valor'] = $_REQUEST [$esteCampo];
