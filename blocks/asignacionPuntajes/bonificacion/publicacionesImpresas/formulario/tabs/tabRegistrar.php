c870bc302d994dafc7b3d25060c8dfba1b61cbc1
Jorge Useche <naturalmentejorge@gmail.com>
2017-10-13 16:49:34 -0500

    Se soluciona problema con tipo de dato, no aceptaba decimales a pesar de ser doble.

--- blocks/asignacionPuntajes/bonificacion/publicacionesImpresas/formulario/tabs/tabRegistrar.php
+++ blocks/asignacionPuntajes/bonificacion/publicacionesImpresas/formulario/tabs/tabRegistrar.php
@@ -493,7 +493,7 @@ class FormularioRegistro {
 		$atributos ['dobleLinea'] = 0;
 		$atributos ['tabIndex'] = $tab;
 		$atributos ['etiqueta'] = $this->lenguaje->getCadena ( $esteCampo );
-		$atributos ['validar'] = 'required, custom[number], custom[onlyNumberSp]';
+		$atributos ['validar'] = 'required, custom[number]';
 			
 		if (isset ( $_REQUEST [$esteCampo] )) {
 			$atributos ['valor'] = $_REQUEST [$esteCampo];
