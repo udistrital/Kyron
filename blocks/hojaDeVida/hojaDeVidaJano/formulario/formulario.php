8008b249bf79ac27800e265e965d006f01f8b0f8
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-03-13 23:19:00 -0500

    Se arreglan algunos errores.

--- blocks/hojaDeVida/hojaDeVidaJano/formulario/formulario.php
+++ blocks/hojaDeVida/hojaDeVidaJano/formulario/formulario.php
@@ -1,30 +1,281 @@
 <?php
-
-
 if (! isset ( $GLOBALS ["autorizado"] )) {
 	include ("../index.php");
 	exit ();
 }
-/**
- * Este script está incluido en el método html de la clase Frontera.class.php.
- *
- * La ruta absoluta del bloque está definida en $this->ruta
- */
+class Formulario {
+	var $miConfigurador;
+	var $lenguaje;
+	var $miFormulario;
+	var $miSql;
+	var $miSesion;
+	var $miSesionSso;
+	function __construct($lenguaje, $formulario, $sql) {
+		$this->miConfigurador = \Configurador::singleton ();
+		
+		$this->miConfigurador->fabricaConexiones->setRecursoDB ( 'principal' );
+		
+		$this->lenguaje = $lenguaje;
+		
+		$this->miFormulario = $formulario;
+		
+		$this->miSql = $sql;
+		
+		$this->miSesion = \Sesion::singleton ();
+		
+		$this->miSesionSso = \SesionSso::singleton ();
+	}
+	function formulario() {
+		$respuesta = $this->miSesionSso->getParametrosSesionAbierta ();
+		
+		$perfiles = $respuesta ['perfil'];
+		
+		// Rescatar los datos de este bloque
+		$esteBloque = $this->miConfigurador->getVariableConfiguracion ( 'esteBloque' );
+		$miPaginaActual = $this->miConfigurador->getVariableConfiguracion ( 'pagina' );
+		
+		$directorio = $this->miConfigurador->getVariableConfiguracion ( 'host' );
+		$directorio .= $this->miConfigurador->getVariableConfiguracion ( 'site' ) . '/index.php?';
+		$directorio .= $this->miConfigurador->getVariableConfiguracion ( 'enlace' );
+		
+		$rutaUrlBloque = $this->miConfigurador->getVariableConfiguracion ( 'host' );
+		$rutaUrlBloque .= $this->miConfigurador->getVariableConfiguracion ( 'site' ) . '/blocks/';
+		$rutaUrlBloque .= $esteBloque ['grupo'] . '/' . $esteBloque ['nombre'];
+		
+		$rutaSara = $this->miConfigurador->getVariableConfiguracion ( 'raizDocumento' );
+		$rutaBloque = $this->miConfigurador->getVariableConfiguracion ( 'rutaBloque' );
+		
+		// -------------------------------------------------------------------------------------------------
+		$atributosGlobales ['campoSeguro'] = 'true';
+		$_REQUEST ['tiempo'] = time ();
+		
+		// ---------------- SECCION: Parámetros Generales del Formulario ----------------------------------
+		$esteCampo = $esteBloque ['nombre'];
+		$atributos ['id'] = $esteCampo;
+		$atributos ['nombre'] = $esteCampo;
+		
+		// Si no se coloca, entonces toma el valor predeterminado 'application/x-www-form-urlencoded'
+		$atributos ['tipoFormulario'] = 'multipart/form-data';
+		
+		// Si no se coloca, entonces toma el valor predeterminado 'POST'
+		$atributos ['metodo'] = 'POST';
+		
+		// Si no se coloca, entonces toma el valor predeterminado 'index.php' (Recomendado)
+		$atributos ['action'] = 'index.php';
+		$atributos ['titulo'] = $this->lenguaje->getCadena ( $esteCampo );
+		
+		// Si no se coloca, entonces toma el valor predeterminado.
+		$atributos ['estilo'] = '';
+		$atributos ['marco'] = true;
+		$tab = 1;
+		
+		// ---------------- FIN SECCION: de Parámetros Generales del Formulario ----------------------------
+		
+		// ----------------INICIAR EL FORMULARIO ------------------------------------------------------------
+		
+		$atributos ['tipoEtiqueta'] = 'inicio';
+		// Aplica atributos globales al control
+		echo $this->miFormulario->formulario ( $atributos );
+		unset ( $atributos );
+		
+		// ---------------- CONTROL: Lista Docente--------------------------------------------------------
+		
+		$esteCampo = 'docente';
+		
+		$atributos ['id'] = $esteCampo;
+		$atributos ['nombre'] = $esteCampo;
+		$atributos ['tipo'] = 'text';
+		$atributos ['estilo'] = 'jqueryui';
+		$atributos ['marco'] = true;
+		$atributos ['estiloMarco'] = '';
+		$atributos ["etiquetaObligatorio"] = false;
+		$atributos ['columnas'] = 1;
+		$atributos ['dobleLinea'] = 0;
+		$atributos ['tabIndex'] = $tab;
+		$atributos ['etiqueta'] = $this->lenguaje->getCadena ( $esteCampo );
+		$atributos ['validar'] = '';
+		$atributos ['textoFondo'] = 'Ingrese Mínimo 3 Caracteres de Búsqueda';
+		
+		if (isset ( $_REQUEST [$esteCampo] )) {
+			$atributos ['valor'] = $_REQUEST [$esteCampo];
+		} else {
+			$atributos ['valor'] = '';
+		}
+		$atributos ['titulo'] = $this->lenguaje->getCadena ( $esteCampo . 'Titulo' );
+		$atributos ['deshabilitado'] = false;
+		$atributos ['tamanno'] = 80;
+		$atributos ['maximoTamanno'] = '';
+		$atributos ['anchoEtiqueta'] = 280;
+		$tab ++;
+		
+		// Aplica atributos globales al control
+		$atributos = array_merge ( $atributos, $atributosGlobales );
+		echo $this->miFormulario->campoCuadroTexto ( $atributos );
+		unset ( $atributos );
+		
+		$esteCampo = 'id_docente';
+		$atributos ["id"] = $esteCampo; // No cambiar este nombre
+		$atributos ["tipo"] = "hidden";
+		$atributos ['estilo'] = '';
+		$atributos ["obligatorio"] = false;
+		$atributos ['marco'] = true;
+		$atributos ["etiqueta"] = "";
+		if (isset ( $_REQUEST [$esteCampo] )) {
+			$atributos ['valor'] = $_REQUEST [$esteCampo];
+		} else {
+			$atributos ['valor'] = '';
+		}
+		$atributos = array_merge ( $atributos, $atributosGlobales );
+		echo $this->miFormulario->campoCuadroTexto ( $atributos );
+		unset ( $atributos );
+		
+		// ----------------FIN CONTROL: Lista Docente--------------------------------------------------------
+		
+		// ------------------Division para los botones-------------------------
+		$atributos ["id"] = "botones";
+		$atributos ["estilo"] = "marcoBotones";
+		echo $this->miFormulario->division ( "inicio", $atributos );
+		
+		// -----------------CONTROL: Botón ----------------------------------------------------------------
+		$esteCampo = 'botonConsultar';
+		$atributos ["id"] = $esteCampo;
+		$atributos ["tabIndex"] = $tab;
+		$atributos ["tipo"] = 'boton';
+		// submit: no se coloca si se desea un tipo button genérico
+		$atributos ['submit'] = 'true';
+		$atributos ["estiloMarco"] = '';
+		$atributos ["estiloBoton"] = 'jqueryui';
+		// verificar: true para verificar el formulario antes de pasarlo al servidor.
+		$atributos ["verificar"] = '';
+		$atributos ["tipoSubmit"] = 'jquery'; // Dejar vacio para un submit normal, en este caso se ejecuta la función submit declarada en ready.js
+		$atributos ["valor"] = $this->lenguaje->getCadena ( $esteCampo );
+		$atributos ['nombreFormulario'] = $esteBloque ['nombre'];
+		$tab ++;
+		
+		// Aplica atributos globales al control
+		$atributos = array_merge ( $atributos, $atributosGlobales );
+		echo $this->miFormulario->campoBoton ( $atributos );
+	
+		// -----------------FIN CONTROL: Botón -----------------------------------------------------------
+	
+		// ------------------Fin Division para los botones-------------------------
+		echo $this->miFormulario->division ( "fin" );
+		
+		// ------------------- SECCION: Paso de variables ------------------------------------------------
+		
+		/**
+		 * En algunas ocasiones es útil pasar variables entre las diferentes páginas.
+		 * SARA permite realizar esto a través de tres
+		 * mecanismos:
+		 * (a). Registrando las variables como variables de sesión. Estarán disponibles durante toda la sesión de usuario. Requiere acceso a
+		 * la base de datos.
+		 * (b). Incluirlas de manera codificada como campos de los formularios. Para ello se utiliza un campo especial denominado
+		 * formsara, cuyo valor será una cadena codificada que contiene las variables.
+		 * (c) a través de campos ocultos en los formularios. (deprecated)
+		 */
+		// En este formulario se utiliza el mecanismo (b) para pasar las siguientes variables:
+		// Paso 1: crear el listado de variables
+		
+		$valorCodificado = "pagina=" . $this->miConfigurador->getVariableConfiguracion ( 'pagina' );
+		// $valorCodificado .= "&bloque=" . $esteBloque ['nombre'];
+		// $valorCodificado .= "&bloqueGrupo=" . $esteBloque ["grupo"];
+		// $valorCodificado .= "&opcion=consultar";
+		/**
+		 * SARA permite que los nombres de los campos sean dinámicos.
+		 * Para ello utiliza la hora en que es creado el formulario para
+		 * codificar el nombre de cada campo.
+		 */
+		$valorCodificado .= "&campoSeguro=" . $_REQUEST ['tiempo'];
+		$valorCodificado .= "&tiempo=" . time ();
+		
+		/*
+		 * Sara permite validar los campos en el formulario o funcion destino.
+		 * Para ello se envía los datos atributos["validadar"] de los componentes del formulario
+		 * Estos se pueden obtener en el atributo $this->miFormulario->validadorCampos del formulario
+		 * La función $this->miFormulario->codificarCampos() codifica automáticamente el atributo validadorCampos
+		 */
+		$valorCodificado .= "&validadorCampos=" . $this->miFormulario->codificarCampos ();
+		
+		// Paso 2: codificar la cadena resultante
+		$valorCodificado = $this->miConfigurador->fabricaConexiones->crypto->codificar ( $valorCodificado );
+		
+		$atributos ["id"] = "formSaraData"; // No cambiar este nombre
+		$atributos ["tipo"] = "hidden";
+		$atributos ['estilo'] = '';
+		$atributos ["obligatorio"] = false;
+		$atributos ['marco'] = true;
+		$atributos ["etiqueta"] = "";
+		$atributos ["valor"] = $valorCodificado;
+		echo $this->miFormulario->campoCuadroTexto ( $atributos );
+		unset ( $atributos );
+		
+		$atributos ['marco'] = true;
+		$atributos ['tipoEtiqueta'] = 'fin';
+		echo $this->miFormulario->formulario ( $atributos );
+		
+		// ----------------FIN SECCION: Paso de variables -------------------------------------------------
+		// ---------------- FIN SECCION: Controles del Formulario -------------------------------------------
+		// ----------------FINALIZAR EL FORMULARIO ----------------------------------------------------------
+		// Se debe declarar el mismo atributo de marco con que se inició el formulario.
+		
+		
+		// ---------------- CONSULTA: Datos Docente --------------------------------------------------------
+		//Si no existe el parámetro docente simplemente imprime el formulario
+		if(!isset($_REQUEST['id_docente'])){
+			return 0;
+		}
+		
+		$documento = $_REQUEST['id_docente'];
+		
+// 		$conexion = 'docencia';
+// 		$esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB ( $conexion );
+		
+// 		$cadenaSql = $this->miSql->getCadenaSql ( 'datos_docente', $documento );
+// 		$resultado = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, 'busqueda' );
+// 		if(!$resultado){
+// 			echo '<h2>No hay datos de este Docente.</h2>';
+// 			return 0;
+// 		}
+// 		$datosDocente = $resultado[0];
+		
+		// ---------------- FIN CONSULTA: Datos Docente --------------------------------------------------------
+		
+		$directorio = '/urano/index.php?data=';
+		
+		$usuario = $this->miSesionSso->getSesionUsuarioId ();
+		
+		$key = $this->miConfigurador->getVariableConfiguracion('keyJano');
+		$key = $this->miConfigurador->fabricaConexiones->crypto->decodificar ( $key );
+		
+		// enlace para visualizar la hoja de vida
+		$variableHojaVida = "pagina=gestionarSoportes";
+		$variableHojaVida .= "&action=gestionarSoportes";
+		$variableHojaVida .= "&bloque=19";
+		$variableHojaVida .= "&bloqueGrupo=";
+		$variableHojaVida .= "&opcion=verHojaVida";
+		$variableHojaVida .= "&campoSeguro=" . $_REQUEST ['tiempo'];
+		$variableHojaVida .= "&tiempo=" . time ();
+		$variableHojaVida .= "&identificacion=" . $documento;
+		$variableHojaVida .= "&usuario=" . $usuario;
+		$variableHojaVida = $directorio . $this->codificar_sara_nuevo( $variableHojaVida, $key );
+		
+		echo '<iframe width="100%" height="1024px" src="' . $variableHojaVida . '"></iframe> ';
+	}
+	
+	function codificar_sara_nuevo($cadena, $semilla) {
+		$cadena = mcrypt_encrypt ( MCRYPT_RIJNDAEL_256, $semilla, $cadena, MCRYPT_MODE_ECB ) ;
+		$cadena=trim($this->base64url_encode($cadena));
+		return $cadena;
+	}
+	
+	function base64url_encode($data) {
+		return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+	}
+}
 
-$esteBloque = $this->miConfigurador->getVariableConfiguracion ( "esteBloque" );
+$miFormulario = new Formulario ( $this->lenguaje, $this->miFormulario, $this->sql );
 
-$nombreFormulario = $esteBloque ["nombre"];
-
-include_once ("core/crypto/Encriptador.class.php");
-$cripto = Encriptador::singleton ();
-$valorCodificado = "action=" . $esteBloque ["nombre"];
-$valorCodificado .= "&bloque=" . $esteBloque ["id_bloque"];
-$valorCodificado .= "&bloqueGrupo=" . $esteBloque ["grupo"];
-
-$valorCodificado = $cripto->codificar ( $valorCodificado );
-$directorio = $this->miConfigurador->getVariableConfiguracion ( "rutaUrlBloque" ) . "/imagen/";
-
+$miFormulario->formulario ();
 ?>
 
- <iframe width="100%" height="1024px"
- src="http://pruebasoas.udistrital.edu.co/jano/index.php?data=zCAKgWGdKovKLs_o6aZrsmD0gs72i7RGYqva23WWhiChon-4JxqZwyLvZ-uOYBpeBzbB5cqPfZAHOnQo8dtg-g"></iframe> 
+ 
